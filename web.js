a0c3a72415411d6a7e1546c7a9cf3f7ad5bb11e4
Rahul <Rahul.Bahl2@gmail.com>
2013-07-27 20:39:17 +0000

    Reverted web.js back to original

--- web.js
+++ web.js
@@ -3,9 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-  readFile = fs.readFileSync(index.html);
-  response.send(new Buffer(readFile, "utf-8").toString("utf-8"));
+  response.send('Hello World 2!');
+
 });
 
 var port = process.env.PORT || 5000;
