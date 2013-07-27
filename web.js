8a88db0a6d56d10395c25cc6e4aa5285f9fb213b
Rahul <Rahul.Bahl2@gmail.com>
2013-07-27 20:23:10 +0000

    TEST

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  readFile = fs.readFileSync(index.html);
+  response.send(new Buffer(readFile, "utf-8").toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
